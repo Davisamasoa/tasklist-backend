c27597b2e97a57171367ad818d29ec9c1803ea38
Davi Samuel <davisamasoa@gmail.com>
2023-09-26 20:58:59 -0300

    fix: database url changed

--- database.js
+++ database.js
@@ -6,8 +6,6 @@ const dbUser = process.env.DB_USERNAME;
 const dbPassword = process.env.DB_PASSWORD;
 
 mongoose
-	.connect(
-		`mongodb+srv://${dbUser}:${dbPassword}@cluster0.okyfezv.mongodb.net/tasklist?retryWrites=true&w=majority`
-	)
+	.connect(`mongodb+srv://${dbUser}:${dbPassword}@cluster0.nkrryo4.mongodb.net/`)
 	.then(() => console.log("Conectado ao MongoDB"))
 	.catch((error) => console.log(error));
